e9bed3e363648471bbc95ce0704952528b760d92
cheva <chevautomo@gmail.com>
2026-04-28 20:53:06 +0700

    update : logic + UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar      from './components/Navbar';
 import UsersPage   from './pages/usersPage';
