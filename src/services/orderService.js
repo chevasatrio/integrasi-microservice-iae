2ebb07a19875658d586f46dd45e811c869faec03
chevasatrio <chevaartemis76@gmail.com>
2026-05-09 14:28:02 +0700

    cleanup duplicate

--- src/services/orderService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE = 'http://localhost:8003/api';
-
-export const getOrders      = ()     => axios.get(`${BASE}/orders`);
-export const getOrderById   = (id)   => axios.get(`${BASE}/orders/${id}`);
-export const createOrder    = (data) => axios.post(`${BASE}/orders`, data);
-export const updateStatus   = (id, status) => axios.put(`${BASE}/orders/${id}/status`, { status });
-export const deleteOrder    = (id)   => axios.delete(`${BASE}/orders/${id}`);
